861745ea6786265fc75af4f125f1e081f95b1b9c
mAssonwilliams <darya.gorat@mail.ru>
2026-09-09 21:31:49 +0500

    Task1

---
+++ Tyuiu.GoreikoDV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GoreikoDV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Dasha";
+            int age = 20;
+            Console.WriteLine("Hello my name is " + name + " me " + age + " years!");
+            Console.ReadKey();
+        }
+    }
+}
